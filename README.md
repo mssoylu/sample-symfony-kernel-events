5bf8fb48cfc86c609b6c75217af1e640573d2c8a
mssoylu <mehmet@soylu.net>
2021-02-02 22:35:34 +0300

    Exception listener default method onKernelException ve service.yaml icinde ozelMethod cagiris

--- README.md
+++ README.md
@@ -12,3 +12,19 @@ https://symfony.com/doc/current/reference/events.html
 - kernel.finish_request
 - kernel.terminate
 - kernel.exception
+
+## Nasil denerim
+
+Bunu denemek icin;
+
+`cd public`
+`php -S localhost:8000`
+uygulamayi php server ile calistirin.
+
+## Kernel Exception
+`kernel.exception` olusturuldugunda tum exceptionlari yakaliyor.
+
+`http://localhost:8000/olmayanroute` adresine 
+giderseniz `dev` moddaki klasik symfony `web_profiler` olan hata sayfasi yerine `ExceptionListener`'in hatayi yakalayip degistirerek
+gosterdigi ekrani goreceksiniz.
+
--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -8,7 +8,41 @@ use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 class ExceptionListener
 {
+    /**
+     * @param ExceptionEvent $event
+     */
     public function onKernelException(ExceptionEvent $event)
+    {
+        // Bir nesne exception gonderirse yakalanir
+        $exception = $event->getThrowable();
+        $message = sprintf(
+            'Mesaj soyle oluyor: %s hata kodu boyle: %s',
+            $exception->getMessage(),
+            $exception->getCode()
+        );
+
+        // Bu exception hatasini daha duzgun gostermek icin response nesnesi duzenlenir
+        $response = new Response();
+        $response->setContent($message);
+
+        // HttpExceptionInterface icin status kod aynen tutulur
+        // header bilgilerinde oldugu gibi yer alir
+        if ($exception instanceof HttpExceptionInterface) {
+            $response->setStatusCode($exception->getStatusCode());
+            $response->headers->replace($exception->getHeaders());
+        } else {
+            // Bu hata baska bir sekilde olusmussa sunucu hatasi yani 500 olarak verilir
+            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        // response duzenlenmis sekilde gonderilir
+        $event->setResponse($response);
+    }
+
+    /**
+     * @param ExceptionEvent $event
+     */
+    public function ozelMethod(ExceptionEvent $event)
     {
         // Bir nesne exception gonderirse yakalanir
         $exception = $event->getThrowable();
